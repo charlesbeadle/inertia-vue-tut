d25daff2ccd0aedaad143d93a606863623f86ab0
Charles Beadle <charles.beadle@protonmail.com>
2022-09-07 06:13:52 -0400

    Init commit

---
+++ resources/views/Pages/Welcome.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+//
+</script>
+
+<template layout="default">
+  <h1>Working example</h1>
+</template>
+
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Laravel with Inertia</title>
+		@vite
+	</head>
+	<body>
+		@inertia
+	</body>
+</html>
